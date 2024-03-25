e69a97977365a1d865fa8d34ca97c1d05544c8cc
sushmitha-666 <164494460+sushmitha-666@users.noreply.github.com>
2024-03-25 14:33:44 +0530

    First project on Paper Rock Scissor

---
+++ project/source code/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Rou1Component } from './rou1/rou1.component';
+import { MainComComponent } from './main-com/main-com.component';
+import { Header1Component } from './header1/header1.component';
+import { Footer1Component } from './footer1/footer1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Rou1Component,
+    MainComComponent,
+    Header1Component,
+    Footer1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
